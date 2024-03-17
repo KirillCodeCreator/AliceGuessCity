b5772bb417756acf9588ca1652e43cad736b74cd
Kirill <okaydiscord@mail.ru>
2024-03-17 18:56:20 +0300

    init app port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import random
 
 from flask import Flask, request, jsonify
@@ -171,4 +172,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
